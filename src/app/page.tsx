2bd359274ce69325219a79fdc6c6c01c5e525ef3
Hung Doan <hungdoansy@gmail.com>
2024-01-25 21:13:08 +0700

    implement NewEra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header"
 import MetaverseExplorer from "@/components/MetaverseExplorer"
+import NewEra from "@/components/NewEra"
 
 export default function Home() {
     return (
         <>
             <Header />
             <MetaverseExplorer />
+            <NewEra />
         </>
     )
 }
---
+++ src/components/NewEra.tsx
@@ -0,0 +1,256 @@
+/* eslint-disable @next/next/no-img-element */
+import localFont from "next/font/local"
+
+const fontClash = localFont({ src: "../fonts/ClashGrotesk-Semibold.woff2" })
+
+const NewEra = () => {
+    return (
+        <div className="bg-bg-darker text-center">
+            <div className="container relative mx-auto px-4 pt-32 lg:pt-48">
+                ƒ
+                <div className="absolute inset-0 w-full">
+                    <span
+                        style={{
+                            boxSizing: "border-box",
+                            display: "inline-block",
+                            overflow: "hidden",
+                            width: "initial",
+                            height: "initial",
+                            background: "none",
+                            opacity: 1,
+                            border: 0,
+                            margin: 0,
+                            padding: 0,
+                            position: "relative",
+                            maxWidth: "100%",
+                        }}
+                    >
+                        <span
+                            style={{
+                                boxSizing: "border-box",
+                                display: "block",
+                                width: "initial",
+                                height: "initial",
+                                background: "none",
+                                opacity: 1,
+                                border: 0,
+                                margin: 0,
+                                padding: 0,
+                                maxWidth: "100%",
+                            }}
+                        >
+                            <img
+                                alt=""
+                                aria-hidden="true"
+                                src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%271280%27%20height=%271280%27/%3e"
+                                style={{
+                                    display: "block",
+                                    maxWidth: "100%",
+                                    width: "initial",
+                                    height: "initial",
+                                    background: "none",
+                                    opacity: 1,
+                                    border: 0,
+                                    margin: 0,
+                                    padding: 0,
+                                }}
+                            />
+                        </span>
+                        <img
+                            alt="Planet"
+                            draggable="false"
+                            srcSet="/bgHeroPlanet.svg 1x, /bgHeroPlanet.svg 2x"
+                            src="/bgHeroPlanet.svg"
+                            decoding="async"
+                            data-nimg="intrinsic"
+                            className="pointer-events-none object-cover object-center"
+                            style={{
+                                position: "absolute",
+                                inset: 0,
+                                boxSizing: "border-box",
+                                padding: 0,
+                                border: "none",
+                                margin: "auto",
+                                display: "block",
+                                width: 0,
+                                height: 0,
+                                minWidth: "100%",
+                                maxWidth: "100%",
+                                minHeight: "100%",
+                                maxHeight: "100%",
+                            }}
+                        />
+                    </span>
+                </div>
+                <div className="relative z-10 text-center">
+                    <span className="inline-block rounded-full border py-1 px-3 text-base font-semibold tracking-wide text-contrast-higher">
+                        light.so
+                    </span>
+                    <h2
+                        className={[
+                            "mt-3 text-4xl font-extrabold text-contrast-higher sm:tracking-tight xl:text-7xl",
+                            fontClash.className,
+                        ].join(" ")}
+                    >
+                        Enter a new era
+                        <br />
+                        of Web 3.0
+                    </h2>
+                    <p className="mx-auto mt-5 max-w-xl text-lg text-contrast-medium md:text-2xl">
+                        We enable users to curate, discover, and explore meaningful interactions on the metaverse in a
+                        way that was not possible before.
+                    </p>
+                </div>
+                <div className="relative">
+                    <div className="absolute inset-0 z-10 my-auto">
+                        <span
+                            style={{
+                                boxSizing: "border-box",
+                                display: "inline-block",
+                                overflow: "hidden",
+                                width: "initial",
+                                height: "initial",
+                                background: "none",
+                                opacity: 1,
+                                border: 0,
+                                margin: 0,
+                                padding: 0,
+                                position: "relative",
+                                maxWidth: "100%",
+                            }}
+                        >
+                            <span
+                                style={{
+                                    boxSizing: "border-box",
+                                    display: "block",
+                                    width: "initial",
+                                    height: "initial",
+                                    background: "none",
+                                    opacity: 1,
+                                    border: 0,
+                                    margin: 0,
+                                    padding: 0,
+                                    maxWidth: "100%",
+                                }}
+                            >
+                                <img
+                                    alt=""
+                                    aria-hidden="true"
+                                    src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27960%27%20height=%27960%27/%3e"
+                                    style={{
+                                        display: "block",
+                                        maxWidth: "100%",
+                                        width: "initial",
+                                        height: "initial",
+                                        background: "none",
+                                        opacity: 1,
+                                        border: 0,
+                                        margin: 0,
+                                        padding: 0,
+                                    }}
+                                />
+                            </span>
+                            <img
+                                alt="Enter a new era"
+                                draggable="false"
+                                src="/imgAbout.webp"
+                                decoding="async"
+                                data-nimg="intrinsic"
+                                style={{
+                                    position: "absolute",
+                                    inset: 0,
+                                    boxSizing: "border-box",
+                                    padding: 0,
+                                    border: "none",
+                                    margin: "auto",
+                                    display: "block",
+                                    width: 0,
+                                    height: 0,
+                                    minWidth: "100%",
+                                    maxWidth: "100%",
+                                    minHeight: "100%",
+                                    maxHeight: "100%",
+                                }}
+                            />
+                        </span>
+                    </div>
+                    <div className="relative inset-0 z-0">
+                        <span
+                            style={{
+                                boxSizing: "border-box",
+                                display: "inline-block",
+                                overflow: "hidden",
+                                width: "initial",
+                                height: "initial",
+                                background: "none",
+                                opacity: 1,
+                                border: 0,
+                                margin: 0,
+                                padding: 0,
+                                position: "relative",
+                                maxWidth: "100%",
+                            }}
+                        >
+                            <span
+                                style={{
+                                    boxSizing: "border-box",
+                                    display: "block",
+                                    width: "initial",
+                                    height: "initial",
+                                    background: "none",
+                                    opacity: 1,
+                                    border: 0,
+                                    margin: 0,
+                                    padding: 0,
+                                    maxWidth: "100%",
+                                }}
+                            >
+                                <img
+                                    alt=""
+                                    aria-hidden="true"
+                                    src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27960%27%20height=%27960%27/%3e"
+                                    style={{
+                                        display: "block",
+                                        maxWidth: "100%",
+                                        width: "initial",
+                                        height: "initial",
+                                        background: "none",
+                                        opacity: 1,
+                                        border: 0,
+                                        margin: 0,
+                                        padding: 0,
+                                    }}
+                                />
+                            </span>
+                            <img
+                                alt="Enter a new era"
+                                draggable="false"
+                                srcSet="/imgBgAbout.svg 1x, /imgBgAbout.svg 2x"
+                                src="/imgBgAbout.svg"
+                                decoding="async"
+                                data-nimg="intrinsic"
+                                style={{
+                                    position: "absolute",
+                                    inset: 0,
+                                    boxSizing: "border-box",
+                                    padding: 0,
+                                    border: "none",
+                                    margin: "auto",
+                                    display: "block",
+                                    width: 0,
+                                    height: 0,
+                                    minWidth: "100%",
+                                    maxWidth: "100%",
+                                    minHeight: "100%",
+                                    maxHeight: "100%",
+                                }}
+                            />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewEra
